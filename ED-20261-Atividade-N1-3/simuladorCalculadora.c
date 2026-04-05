17854fae2c77786af657dcfb24002876832959ed
Miittsuki <112829737+MilenaSayuri@users.noreply.github.com>
2026-04-05 18:12:26 -0300

    Correcting the calculator simulator code (atv3)

--- ED-20261-Atividade-N1-3/simuladorCalculadora.c
+++ ED-20261-Atividade-N1-3/simuladorCalculadora.c
@@ -3,95 +3,78 @@
 #include <string.h>
 #include <ctype.h>
 
-typedef struct
-{
-    double x, y, z, t;
-} HP12c;
+#define TAM_PILHA 4
 
-void exibirPilha(HP12c *p)
-{
-    printf("\n--- ESTADO DOS REGISTRADORES ---");
-    printf("\n T: %10.2f", p->t);
-    printf("\n Z: %10.2f", p->z);
-    printf("\n Y: %10.2f", p->y);
-    printf("\n X: %10.2f (Display)", p->x);
-    printf("\n--------------------------------\n");
+void exibirPilha(int pilha[]) {
+    printf("\n[T: %d]", pilha[3]);
+    printf("\n[Z: %d]",  pilha[2]);
+    printf("\n[Y: %d]",  pilha[1]);
+    printf("\n[X: %d] (Display)",  pilha[0]);
+    printf("\n------------------------------\n");
 }
 
-void push(HP12c *p, double valor)
-{
-    p->t = p->z;
-    p->z = p->y;
-    p->y = p->x;
-    p->x = valor;
+
+void empurrar(int pilha[], int valor) {
+    pilha[3] = pilha[2];
+    pilha[2] = pilha[1];
+    pilha[1] = pilha[0];
+    pilha[0] = valor;
 }
 
-void executarOperacao(HP12c *p, char op)
-{
-    double resultado = 0;
 
-    switch (op)
-    {
-    case '+':
-        resultado = p->y + p->x;
-        break;
-    case '-':
-        resultado = p->y - p->x;
-        break;
-    case '*':
-        resultado = p->y * p->x;
-        break;
-    case '/':
-        if (p->x != 0)
-            resultado = p->y / p->x;
-        else
-        {
-            printf("\nErro: Divisao por zero!\n");
+void executaOperacao(int pilha[], char op){
+    int resultado = 0;
+    int x = pilha[0];
+    int y = pilha[1];
+   
+    if (op == '+') resultado = y + x;
+    else if (op == '-') resultado = y - x;
+    else if (op == '*') resultado = y * x;
+    else if (op == '/') {
+        if (x != 0) {
+            resultado = y / x;
+        } else {
+            printf("\nErro: Divisao por zero!");
             return;
         }
-        break;
-    default:
-        return;
     }
-
-    p->x = resultado;
-    p->y = p->z;
-    p->z = p->t;
+    
+    pilha[0] = resultado;
+    pilha[1] = pilha[2];
+    pilha[2] = pilha[3];
+    pilha[3] = 0;
 }
 
-int main()
-{
-    HP12c pilha = {0, 0, 0, 0};
-    char entrada[256];
+int main() {
+    int pilha[TAM_PILHA] = {0, 0, 0, 0}; 
+    char entrada[100];
 
     printf("--- SIMULADOR LÓGICO HP12c (RPN) ---\n");
     printf("Digite a expressão RPN (ex: 10 5 + 2 *): ");
-
     fgets(entrada, sizeof(entrada), stdin);
 
     entrada[strcspn(entrada, "\n")] = 0;
 
     char *token = strtok(entrada, " ");
 
-    while (token != NULL)
-    {
-        if (strlen(token) == 1 && strpbrk(token, "+-*/"))
-        {
-            printf("\nOperador detectado: %s", token);
-            executarOperacao(&pilha, token[0]);
-        }
-        else
-        {
-            printf("\nNúmero detectado: %s", token);
-            push(&pilha, atof(token));
+    while (token != NULL) {
+        
+        if (ispunct(token[0]) && strlen(token) == 1) {
+            executaOperacao(pilha, token[0]);
+        } else {
+            
+            empurrar(pilha, atoi(token));
         }
 
-        exibirPilha(&pilha);
+        
+        printf("\nProcessando: %s", token);
+        exibirPilha(pilha);
+
         token = strtok(NULL, " ");
     }
 
     printf("\n====================================");
-    printf("\nO resultado final no console é: %.2f", pilha.x);
+    printf("\nO resultado final no console é: %d", pilha[0]);
     printf("\n====================================\n");
 
     return 0;
